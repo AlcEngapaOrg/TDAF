7156a3ddea2a411f1094337d6811f95415219665
Enrique <engapa.tid@gmail.com>
2013-05-20 14:54:49 +0200

    IPDR Schemas

--- accounting/data/src/test/java/es/tid/cloud/tdaf/accounting/data/PersistEventsRouteTest.java
+++ accounting/data/src/test/java/es/tid/cloud/tdaf/accounting/data/PersistEventsRouteTest.java
@@ -1,18 +1,25 @@
 package es.tid.cloud.tdaf.accounting.data;
 
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class PersistEventsRouteTest extends CamelSpringTestSupport {
 
+	/** The producer template.*/
+    @Produce(uri = "vm:persist-events")
+    private ProducerTemplate template;
+
 	@Override
 	protected AbstractApplicationContext createApplicationContext() {
 		return new ClassPathXmlApplicationContext(
                 new String[] {
+                    "classpath:META-INF/spring/app-context.xml",
                     "classpath:META-INF/spring/persist-context.xml"});
 	}
 
-
+	
 
 }
