116b125341cae4b9c1e4af44cefed84db1365d60
Enrique <engapa.tid@gmail.com>
2013-05-20 15:08:17 +0200

    First test for account data module

--- accounting/data/src/test/java/es/tid/cloud/tdaf/accounting/data/PersistEventsRouteTest.java
+++ accounting/data/src/test/java/es/tid/cloud/tdaf/accounting/data/PersistEventsRouteTest.java
@@ -3,8 +3,11 @@ package es.tid.cloud.tdaf.accounting.data;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mongodb.WriteResult;
 
 public class PersistEventsRouteTest extends CamelSpringTestSupport {
 
@@ -20,6 +23,10 @@ public class PersistEventsRouteTest extends CamelSpringTestSupport {
                     "classpath:META-INF/spring/persist-context.xml"});
 	}
 
-	
+	@Test
+	public void persistEvent(){
+		Object o = template.requestBody(new String("{name:'enrique',lastName:'garcia'}"));
+		assertTrue("Result is not of type WriteResult", o instanceof WriteResult);
+	}
 
 }
